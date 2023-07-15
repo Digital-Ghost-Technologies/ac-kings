3685bc0979591b3b1b5936daa63da3123b5bef97
Brian Seegmiller <brianseegmiller@gmail.com>
2023-07-15 12:25:11 -0700

    home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
-import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+  site: "https://theackings.com",
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false,
+    }
+  }), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })]
 });
